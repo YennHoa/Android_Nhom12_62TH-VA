04007edc6c5faf124acf67d6affafadc9f5edbc6
thutrang18 <Nguyenthutranghd.202@gmail.com>
2023-04-08 17:48:38 +0700

    m

---
+++ Android_Nhom12_62THVA/app/src/main/java/com/example/android_nhom12_62th_va/Activity/productActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_nhom12_62th_va.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class productActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+    }
+}
